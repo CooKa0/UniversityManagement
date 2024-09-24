8adcf6733656fa54f1593d45ff8e3f40f41b91de
CooKa0 <marcinkuj@gmail.com>
2024-09-24 16:40:03 +0200

    -

--- src/main/java/com/solvd/laba/university/utils/ReflectionUtil.java
+++ src/main/java/com/solvd/laba/university/utils/ReflectionUtil.java
@@ -4,6 +4,7 @@ import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
+import java.util.Arrays;
 
 public class ReflectionUtil {
 
@@ -12,24 +13,20 @@ public class ReflectionUtil {
 
         // Print constructors
         System.out.println("\nConstructors:");
-        Constructor<?>[] constructors = clazz.getDeclaredConstructors();
-        for (Constructor<?> constructor : constructors) {
-            System.out.println(" " + constructor);
-        }
+        Arrays.stream(clazz.getDeclaredConstructors())
+                .forEach(constructor -> System.out.println(" " + constructor));
 
         // Print fields
         System.out.println("\nFields:");
-        Field[] fields = clazz.getDeclaredFields();
-        for (Field field : fields) {
-            System.out.println(" " + Modifier.toString(field.getModifiers()) + " " + field.getType().getName() + " " + field.getName());
-        }
+        Arrays.stream(clazz.getDeclaredFields())
+                .forEach(field -> System.out.println(" " + Modifier.toString(field.getModifiers()) + " "
+                        + field.getType().getName() + " " + field.getName()));
 
         // Print methods
         System.out.println("\nMethods:");
-        Method[] methods = clazz.getDeclaredMethods();
-        for (Method method : methods) {
-            System.out.println(" " + Modifier.toString(method.getModifiers()) + " " + method.getReturnType().getName() + " " + method.getName());
-        }
+        Arrays.stream(clazz.getDeclaredMethods())
+                .forEach(method -> System.out.println(" " + Modifier.toString(method.getModifiers()) + " "
+                        + method.getReturnType().getName() + " " + method.getName()));
     }
 
     public static Object createInstance(Class<?> clazz, Class<?>[] paramTypes, Object... params) throws Exception {
